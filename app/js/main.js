ae4a30320d0377893ae5298a840820116b8a1082
Mher-777 <mherjan2003@mail.ru>
2020-01-19 22:21:25 +0400

    added gallery section and plugine mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,27 @@ $(function () {
     
     $('.header__slider').slick({
         arrows: false,
-        fade: true
-    })
+        fade: true,
+        autoplay: true,
+    
+    });
 
+    $('.about-slider').slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        prevArrow: '<button class="about-slider__box-arrows arrow-left"><img src="./images/arrows-left.svg" alt=""></button>',
+        nextArrow: '<button class="about-slider__box-arrows arrow-right"><img src="./images/arrows-right.svg" alt=""></button>',
+    });
+   
+    $('.book-lesson__subscribe-checkbox-wrapper label').on('click', function () {
+        $(this).removeClass('active')
+    });
+
+    $('.gallery-menu__btn').on('click', function () {
+        $('.gallery-menu__btn').removeClass('active');
+        $(this).addClass('active');
+    })
+    var mixer = mixitup('.gallery__wrapper');
 });
