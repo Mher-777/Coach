2827cb8f48327c275c350c95ada99da935be5b87
Mher-777 <mherjan2003@mail.ru>
2020-01-21 17:35:39 +0400

    end desctop version

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,21 @@ $(function () {
         autoplay: true,
     
     });
+    $('.video__review-slider').slick({
+        arrows: true,
+        infinite: false,
+        prevArrow: '<button class="video__review-arrows arrow-left"><img src="./images/arrows-left-black.svg" alt=""></button>',
+        nextArrow: '<button class="video__review-arrows arrow-right"><img src="./images/arrows-right-black.svg" alt=""></button>',
+        
+    });
+    $('.calendar__inner-slider').slick({
+        arrows: true,
+        infinite: true,
+        prevArrow: '<button class="calendar-arrows arrow-left"><img src="./images/arrows-left-white.svg" alt=""></button>',
+        nextArrow: '<button class="calendar-arrows arrow-right"><img src="./images/arrows-right-white.svg" alt=""></button>',
+        appendArrows: '.calendar-item__pagination',
+        rows: 0,
+    });
 
     $('.about-slider').slick({
         arrows: true,
@@ -23,6 +38,10 @@ $(function () {
     $('.gallery-menu__btn').on('click', function () {
         $('.gallery-menu__btn').removeClass('active');
         $(this).addClass('active');
+    });
+
+    $('.faq-question__item').on('click', function () {
+        $(this).toggleClass('active')
     })
     var mixer = mixitup('.gallery__wrapper');
 });
